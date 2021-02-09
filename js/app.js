e60bc6bfb050dd3b01e504b0b2bad24e197bf7cc
mbehi <mohsin.behi@gmail.com>
2021-02-09 02:21:49 -0600

    added code to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 'use strict';
 
 console.log('hello mohsin');
+
+// linter test
+var apple = 42;
+
+if(apple){
+  console.log(apple);
+}
+
+
+console.log(apple);
+
+
+// write 5 questions. They MUST accept yes or no or y or n IN ANY CASE
+// examples: YES, yes, YEs, yeS, yEs, Y, y
+
+var questionOne = prompt('Do I live in Austin?');

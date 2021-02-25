13e5bc6cd8f5126622bb43c2aea04d4926e1ad6c
mbehi <mohsin.behi@gmail.com>
2021-02-25 16:06:38 -0600

    added questions to app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ console.log('hello mohsin');
 // write 5 questions. They MUST accept yes or no or y or n IN ANY CASE
 // examples: YES, yes, YEs, yeS, yEs, Y, y
 
-// get user name and offer greeting
+// First Question: get user name and offer greeting
 var userName = prompt('Hello, what\'s your name?');
 alert ('Hello ' + userName + ' , nice to meet you!');
 
@@ -15,4 +15,44 @@ if(answerOne === 'yes' || answerOne === 'y'){
   // console.log('you are correct');
   alert('you are correct');
 }
-var answerTwo = prompt('')
+
+// Second Question
+var answerTwo = prompt('Do you believe in ghosts?').toLowerCase();
+if(answerTwo === 'yes' || answerTwo === 'y'){
+  // console.log('question two worked');
+  alert('You should join the Ghostbusters!');
+}
+// Third Question
+var answerThree = prompt('Have you ever seen a UFO?').toLowerCase();
+if(answerTwo === 'yes' || answerTwo === 'y'){
+  // console.log('question three worked');
+  alert('You need to visit Roswell, NM!');
+}
+
+// Fourth Question
+var answerThree = prompt('Have you ever seen a UFO?').toLowerCase();
+if(answerTwo === 'yes' || answerTwo === 'y'){
+  // console.log('question three worked');
+  alert('You need to visit Roswell, NM!');
+}
+
+// Fifth Question
+var answerThree = prompt('Have you ever seen a UFO?').toLowerCase();
+if(answerTwo === 'yes' || answerTwo === 'y'){
+  // console.log('question three worked');
+  alert('You need to visit Roswell, NM!');
+}
+
+// Sixth Question
+var answerThree = prompt('Have you ever seen a UFO?').toLowerCase();
+if(answerTwo === 'yes' || answerTwo === 'y'){
+  // console.log('question three worked');
+  alert('You need to visit Roswell, NM!');
+}
+
+// Seventh Question
+var answerThree = prompt('Have you ever seen a UFO?').toLowerCase();
+if(answerTwo === 'yes' || answerTwo === 'y'){
+  // console.log('question three worked');
+  alert('You need to visit Roswell, NM!');
+}
